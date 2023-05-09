9489198b7fbb66f09232c72f5587119011e8f550
marialuiza <maria_luiza1598@hotmail.com>
2023-05-09 16:36:20 -0300

    Code with comments

--- dccthread.c
+++ dccthread.c
@@ -15,18 +15,20 @@
 
 //Thread structure
 typedef struct dccthread{
-    char name[DCCTHREAD_MAX_NAME_SIZE]; //Threads's name
+    char name[DCCTHREAD_MAX_NAME_SIZE];
     ucontext_t *context;
     bool isWaiting;
     dccthread_t *waintingFor;
 } dccthread_t;
 
+// Semaphore structure
 typedef struct bin_sem{
     int flag;
     int guard;
     struct dlist *sleepingThreadList;
 } bin_sem_t;
 
+// Global variables
 sigset_t mask;
 sigset_t sleepmask;
 ucontext_t manager;
@@ -35,28 +37,33 @@ bin_sem_t *binSem;
 
 //Function to initialize the thread library and creat a new thread to excecute the function func with the parameter param
 void dccthread_init(void (*func)(int), int param){
+
+    // Create the ready list
     readyThreadList = dlist_create();
+
+    // Initialize the sleeping semaphore
     binSem =  malloc(sizeof(bin_sem_t));
     binSem->sleepingThreadList = dlist_create();
     binSem->flag = 0;
     binSem->guard = 0;
-    // 1 -- Initialize a meneger thread that will scalonate the new threads 
-    // 2 -- Inicialize a main thread that will execute func THE NAME HAS TO BE MAIN
+
+    // Create the main thread
     dccthread_create("main", func, param);
     getcontext(&manager);
 
-    // set timer
+    // Set timer and signal
     timer_t timer_id;
     struct sigevent se;
     struct sigaction sa;
     struct itimerspec ts;
 
+    sa.sa_flags = 0;
     se.sigev_signo = SIGRTMIN;
     se.sigev_notify = SIGEV_SIGNAL;
     se.sigev_notify_attributes = NULL;
     se.sigev_value.sival_ptr = &timer_id;
-    sa.sa_flags = 0;
-    sa.sa_handler = (void *)dccthread_yield;
+    
+    sa.sa_sigaction = (void *)dccthread_yield;
 
     ts.it_interval.tv_sec = TIMER_INTERVAL_SEC;
     ts.it_interval.tv_nsec = TIMER_INTERVAL_NSEC;
@@ -72,65 +79,84 @@ void dccthread_init(void (*func)(int), int param){
     timer_create(CLOCK_PROCESS_CPUTIME_ID, &se, &timer_id);
     timer_settime(timer_id, 0, &ts, NULL);
 
-    // 3 -- Execute the main thread
-    while(!dlist_empty(readyThreadList) || !dlist_empty(binSem->sleepingThreadList))
-    {
-        sigprocmask(SIG_UNBLOCK, &sleepmask, NULL);
-        sigprocmask(SIG_BLOCK, &sleepmask, NULL);
-        dccthread_t *nextThread = (dccthread_t*) dlist_get_index(readyThreadList, 0); // get the next in line thread
+    
+    // Run the threads
+    while(!dlist_empty(readyThreadList) || !dlist_empty(binSem->sleepingThreadList)){
 
-        if(nextThread->isWaiting)
-        {
+        // Unblock the sleeping signal
+        sigprocmask(SIG_UNBLOCK, &sleepmask, NULL);
+
+        // Block the sleeping signal
+        sigprocmask(SIG_BLOCK, &sleepmask, NULL);
+
+        // Get the next thread to be executed
+        dccthread_t *nextThread = (dccthread_t*) dlist_get_index(readyThreadList, 0);
+
+        // If the next thread is waiting, put it at the end of the list
+        if(nextThread->isWaiting){
             dlist_pop_left(readyThreadList);
             dlist_push_right(readyThreadList, nextThread);
             continue;
         }
 
+        // Swap the context to the next thread's context
         swapcontext(&manager, nextThread->context);
+
+        // Remove the thread from the ready list
         dlist_pop_left(readyThreadList);
 
+        // If the thread is waiting, put it at the end of the list
         if(nextThread->isWaiting){
             dlist_push_right(readyThreadList, nextThread);
         }
     }
+
+    // Destroy the timer
     timer_delete(timer_id);
+
+    // Destroy the ready list
     dlist_destroy(readyThreadList, NULL);
+
+    // Destroy the sleeping list
     dlist_destroy(binSem->sleepingThreadList, NULL);
+
     exit(0); 
 }
 
-//Create a new thread with name that will execute func with param
+//Create a new thread to execute the function func with the parameter param
 dccthread_t * dccthread_create(const char *name, void (*func)(int), int param){
 
-    // 1 -- Inicialize a new thread 
+    // Alloc memory for the thread and its context
+    dccthread_t *newThread = malloc(sizeof(dccthread_t));
+    ucontext_t *newContext = malloc(sizeof(ucontext_t));
 
-        // Inicialize the thread
-        dccthread_t *newThread = malloc(sizeof(dccthread_t));
-        ucontext_t *newContext = malloc(sizeof(ucontext_t));
-        getcontext(newContext);// Initializes the new thread's context as the currently active context
+    // Initialize the new thread's context
+    getcontext(newContext);
 
-        sigprocmask(SIG_BLOCK, &mask, NULL);
-        strcpy(newThread->name, name);
-        newThread->isWaiting = false;
-        newThread->waintingFor = NULL;
+    // Block the signal
+    sigprocmask(SIG_BLOCK, &mask, NULL);
 
-        // Inicialize the context
+    // Inicialize the thread's attributes
+    strcpy(newThread->name, name);
+    newThread->isWaiting = false;
+    newThread->waintingFor = NULL;
 
-        
+    // Inicialize the context's attributes
+    newContext->uc_link = &manager;
+    newContext->uc_stack.ss_flags = 0;
+    newContext->uc_stack.ss_size = THREAD_STACK_SIZE;
+    newContext->uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
 
-        newContext->uc_link = &manager;
-        newContext->uc_stack.ss_flags = 0;
-        newContext->uc_stack.ss_size = THREAD_STACK_SIZE;
-        newContext->uc_stack.ss_sp = malloc(THREAD_STACK_SIZE);
-
-        newThread->context = newContext;
+    // Inicialize the threads' context
+    newThread->context = newContext;
     
-    // 2 -- Add it to the list of threads ready to execute
+    // Add the new thread to the ready list
     dlist_push_right(readyThreadList, newThread);
 
-    // 3 -- Return to the thread that originally called the function
-    makecontext(newThread->context, (void(*)(void))func, 1, param); // criação do contexto da thread
+    // Creating the context for the new thread
+    makecontext(newThread->context, (void(*)(void))func, 1, param);
 
+    // Unblock the signal
     sigprocmask(SIG_UNBLOCK, &mask, NULL);
 
     return newThread;
@@ -138,15 +164,17 @@ dccthread_t * dccthread_create(const char *name, void (*func)(int), int param){
 
 void dccthread_yield(void){
 
+    // Block the signal
     sigprocmask(SIG_BLOCK, &mask, NULL);
 
-    // 1 -- Remove the current thread from the CPU
+    // Return to the end of the list
     dccthread_t *currentThread = dccthread_self();
-    dlist_push_right(readyThreadList, currentThread); // Return to the end of the list
-    // 2 -- Call the maneger thread to choose the next thread
-    swapcontext(currentThread->context, &manager);
-    // * a tread must be executed in the order they were created
+    dlist_push_right(readyThreadList, currentThread);
 
+    // Call the maneger thread to choose the next thread
+    swapcontext(currentThread->context, &manager);
+
+    // Unblock the signal
     sigprocmask(SIG_UNBLOCK, &mask, NULL);
 
 }
@@ -165,19 +193,22 @@ const char *dccthread_name(dccthread_t *tid){
 // Ends the current thread
 void dccthread_exit(void){
 
+    // Block the signal
     sigprocmask(SIG_BLOCK, &mask, NULL);
 
     dccthread_t *currentThread = dccthread_self();
-    for(int i = 0; i < readyThreadList->count; i++)
-    {
+
+    // Find the threads that are waiting for the current thread
+    for(int i = 0; i < readyThreadList->count; i++){
         dccthread_t *thread = dlist_get_index(readyThreadList, i);
         if((thread->isWaiting && thread->waintingFor == currentThread)){
             thread->waintingFor = NULL;
             thread->isWaiting = false;
         }
     }
-    for (int i = 0; i < binSem->sleepingThreadList->count; i++)
-    {
+
+    // Find the sleeping threads that are waiting for the current thread
+    for(int i = 0; i < binSem->sleepingThreadList->count; i++){
         dccthread_t *thread = dlist_get_index(binSem->sleepingThreadList, i);
         if((thread->isWaiting && thread->waintingFor == currentThread)){
             thread->waintingFor = NULL;
@@ -185,23 +216,28 @@ void dccthread_exit(void){
         }
     }
         
+    // Free the memory allocated for the thread
     free(currentThread);
+    
+    // Set context back to the manager thread
     setcontext(&manager);
 
+    // Unblock the signal
     sigprocmask(SIG_UNBLOCK, &mask, NULL);
 
-} // when the lat thread calls for exit the program must end
+}
 
 // Blocks the current thread untill tid is done executing
 void dccthread_wait(dccthread_t *tid){
 
+    // Block the signal
     sigprocmask(SIG_BLOCK, &mask, NULL);
 
     bool found = false;
 
     dccthread_t *currentThread = dccthread_self();
 
-    // if we find the object Tid in the ready list its not done executing yet
+    // Look for the object Tid in the ready list
     for(int i = 0; i < readyThreadList->count && !found; i++)
     {
         dccthread_t *thread = dlist_get_index(readyThreadList, i);
@@ -211,6 +247,8 @@ void dccthread_wait(dccthread_t *tid){
             break;
         }
     }
+
+    // Look for the object Tid in the sleeping list
     for(int i = 0; i < binSem->sleepingThreadList->count && !found; i++)
     {
         dccthread_t *thread = dlist_get_index(binSem->sleepingThreadList, i);
@@ -221,14 +259,17 @@ void dccthread_wait(dccthread_t *tid){
         }
     }
 
-    // if we found the object Tid in the ready list, then the current thread is going to stop executing and wait fot tid to end
+    // If we found the object Tid in the ready list, then the current thread is going to stop executing and wait fot tid to end
     if(found)
     {
         currentThread->isWaiting = true; 
         currentThread->waintingFor = tid;
+
+        // Call the maneger thread to choose the next thread
         swapcontext(currentThread->context, &manager);
     }
 
+    // Unblock the signal
     sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
 
@@ -242,46 +283,56 @@ void binWait(dccthread_t *thread){
     //while(testAndSet(&binSem->guard) == 1);
     //if(binSem->flag == 0){
     //    binSem->flag = 1;
-    //    binSem->guard = 0;
+    //    binSem->guard = 0; // Libera o loop
     //}
     //else{
-    //    binSem->guard = 1;
-        dlist_push_right(binSem->sleepingThreadList, thread);
-    //    binSem->guard = 0;
+    //    dlist_push_right(binSem->sleepingThreadList, thread);
+    //    binSem->guard = 0; // Libera o loop
     //}
+    binSem->flag--;
+    if(binSem->flag < 0){
+        dlist_push_right(binSem->sleepingThreadList, thread);
+    }
 }
 
 void binSignal(int sig, siginfo_t *si, void *uc){
     //while(testAndSet(&binSem->guard) == 1);
     //dccthread_t *thread;
     //if(dlist_empty(binSem->sleepingThreadList)){
-    //    binSem->flag = 0;
+    //    binSem->flag = 0; // Ninguem está esperando
     //}
     //else{
     //    thread = (dccthread_t *)si->si_value.sival_ptr;
     //    dlist_pop_left(binSem->sleepingThreadList);
     //    dlist_push_right(readyThreadList, thread);
     //}
-    //binSem->guard = 0;
-    dccthread_t* sleeping1 = (dccthread_t *)si->si_value.sival_ptr;
-    dlist_pop_left(binSem->sleepingThreadList);
-    dlist_push_right(readyThreadList, sleeping1);
+    //binSem->guard = 0; // Libera o loop
+    dccthread_t* sleepingThread = (dccthread_t *)si->si_value.sival_ptr;
+    binSem->flag++;
+    if(binSem->flag <= 0){
+        dlist_pop_left(binSem->sleepingThreadList);
+        dlist_push_right(readyThreadList, sleepingThread);
+    }
+    //dlist_pop_left(binSem->sleepingThreadList);
+    //dlist_push_right(readyThreadList, sleeping1);
 }
 
 void dccthread_sleep(struct timespec ts){
+
+    // Block the signal
     sigprocmask(SIG_BLOCK, &mask, NULL);
 
     dccthread_t *currentThread = dccthread_self();
 
-    
+    // Acquire the semaphore and put the thread to sleep
+    binWait(currentThread);
+
+    // Set the timer
     timer_t timerp;
     struct sigaction s1;
     struct sigevent sev;
     struct itimerspec its;
 
-    //dlist_push_right(ready_list, current_thread);
-
-    
     s1.sa_flags = SA_SIGINFO;
     s1.sa_sigaction = binSignal;
     s1.sa_mask = mask;
@@ -297,7 +348,9 @@ void dccthread_sleep(struct timespec ts){
     its.it_interval.tv_nsec = 0;
     timer_settime(timerp, 0, &its, NULL);
 
-    dlist_push_right(binSem->sleepingThreadList, currentThread);
+    // Call the maneger thread to choose the next thread
     swapcontext(currentThread->context, &manager);
+
+    // Unblock the signal
     sigprocmask(SIG_UNBLOCK, &mask, NULL);
 }
